63ae93c49e240660724f9fcd144aca85a71474a5
Ibrahim-Siddique <139663719+Ibrahim-Siddique@users.noreply.github.com>
2024-10-05 13:56:17 -0400

    sdfljsdfj

--- components/RecordAudio.tsx
+++ components/RecordAudio.tsx
@@ -7,38 +7,34 @@ import SpeechRecognition, {
 } from "react-speech-recognition";
 
 const RecordAudio = () => {
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState("");
+
+  const audioCallback = (command: string, spokenPhrase: string, similarityRatio: number) => {
+    setMessage(
+      `${command} and ${spokenPhrase} are ${similarityRatio * 100}% similar`
+    );
+  };
 
   const commands = [
     {
-      command: 'I would like to order *',
-      callback: (food) => setMessage(`Your order is for: ${food}`)
-    },
-    {
-      command: 'Beijing',
-      callback: (command, spokenPhrase, similarityRatio) => setMessage(`${command} and ${spokenPhrase} are ${similarityRatio * 100}% similar`),
-      // If the spokenPhrase is "Benji", the message would be "Beijing and Benji are 40% similar"
-      isFuzzyMatch: true,
-      fuzzyMatchingThreshold: 0.2
-    },
-    {
-      command: ['eat', 'sleep', 'leave'],
-      callback: (command) => setMessage(`Best matching command: ${command}`),
+      command: "hey jellybean *",
+      callback: audioCallback,
       isFuzzyMatch: true,
       fuzzyMatchingThreshold: 0.2,
-      bestMatchOnly: true
     },
-    {
-      command: 'clear',
-      callback: ({ resetTranscript }) => resetTranscript()
-    }
-  ]
+  ];
 
-  const { transcript, listening, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition({ commands });
-  SpeechRecognition.startListening({ continuous: true })
+  const {
+    transcript,
+    listening,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition({ commands });
+
+  // continuous listening
+  SpeechRecognition.startListening({ continuous: true });
 
   if (!browserSupportsSpeechRecognition) {
-    return <p>Browser doesn't support speech recognition.</p>
+    return <p>Browser doesn't support speech recognition.</p>;
   }
 
   return (
