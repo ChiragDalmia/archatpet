14bee576a421420e8f86a22475bb33ef0567cb92
Ibrahim-Siddique <139663719+Ibrahim-Siddique@users.noreply.github.com>
2024-10-05 18:02:50 -0400

    stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,13 @@
 import PetModelCreator from "@/components/pet-model-creator";
-<<<<<<< HEAD
-=======
 import dynamic from "next/dynamic";
 
 const ARScene = dynamic(() => import("@/components/ARScene"), { ssr: false });
->>>>>>> a3e4e21 (Update audiomanager)
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
       <PetModelCreator />
-<<<<<<< HEAD
-     
-      {/* <RecordAudio characterName="John" /> */}
-=======
       <ARScene />
->>>>>>> a3e4e21 (Update audiomanager)
     </main>
   );
 }
